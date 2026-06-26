947a3f60511b9848166bf74f1613c1eeb76c7ece
Alex <alex040892@gmail.com>
2026-06-26 12:21:05 +1200

    responsive stytes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -10,8 +6,6 @@ import Testimonial from './components/Testimonial';
 import About from './components/About';
 import Contact from './components/Contact';
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
